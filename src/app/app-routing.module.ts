6922d947b13c6020a5e7e9b048a769dbf1872ce6
Carlos Mafra <carlosfgmafra@gmail.com>
2023-03-17 13:10:43 -0400

    Componentes das duas paginas para apresentar fora  criados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {DetalhesComponent} from "./detalhes/detalhes.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'',
+    children:[
+      {
+        path:'Home',
+        title:'Home',
+        component: HomeComponent,
+      },
+      {
+        path:'Detalhes',
+        title:'Detalhes',
+        component: DetalhesComponent,
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DetalhesComponent } from './detalhes/detalhes.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetalhesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
